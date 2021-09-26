6bd4bf49bbecebb62712c0c4443b65d00e2d42c5
Aleksandr Ponomarenko <ponomaleks@gmail.com>
2021-09-26 18:24:30 +0300

    styles for task 3

--- src/components/Friend-list/Friend-list-item/FriendListItem.jsx
+++ src/components/Friend-list/Friend-list-item/FriendListItem.jsx
@@ -3,7 +3,7 @@ import s from './FriendListItem.module.css';
 export default function FriendListItem({ isOnline, avatar, name }) {
   return (
     <>
-      <span className={s.status}>{isOnline}</span>
+      <span className={isOnline ? s.true : s.false}>{isOnline}</span>
       <img className={s.avatar} src={avatar} alt="avatar" width="48" />
       <p className={s.name}>{name}</p>
     </>
